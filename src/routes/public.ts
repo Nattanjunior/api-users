fe1527942d0aba66f559485e466e09a9cf6b08c9
nattanjunior <fnatanieljunior@gmail.com>
2024-08-15 17:25:48 -0300

    create: route POST of users

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,16 +1,38 @@
 import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+import z from "zod"
+import bcrypt from "bcrypt";
 
-export async function RoutesPublic(app:FastifyInstance){
+
+export async function RoutesPublic(app: FastifyInstance) {
   // Cadastro
-  app.get('/',(req, res)=>{
-    return {message: "hello"}
-  })
+  app.post('/cadastro', async (req, res) => {
+    try {
+      const bodySchema = z.object({
+        email: z.string().email(),
+        name: z.string(),
+        password: z.string(),
+      })
+      
+      const { email, name, password } = bodySchema.parse(req.body)
 
-  app.post('/cadastro',(req,res)=>{
-    const user = req.body
-    res.status(201)
-    return {message: "user create sucess!", user}
-  }) 
+      const salt = await bcrypt.genSalt(10)
+      const hashPassword = await bcrypt.hash(password, salt)
+
+      const user = await prisma.user.create({
+        data:{
+          email,
+          name,
+          password: hashPassword,
+        }
+      })
+      res.status(201)
+      return { message: "user create sucess!", user }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  })
 }
 
 // fnatanieljunior
