49207831f28b9b6ab525c7792ba46444ae07c119
nattanjunior <fnatanieljunior@gmail.com>
2024-08-15 19:09:37 -0300

    create route login

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -13,14 +13,14 @@ export async function RoutesPublic(app: FastifyInstance) {
         name: z.string(),
         password: z.string(),
       })
-      
+
       const { email, name, password } = bodySchema.parse(req.body)
 
       const salt = await bcrypt.genSalt(10)
       const hashPassword = await bcrypt.hash(password, salt)
 
       const user = await prisma.user.create({
-        data:{
+        data: {
           email,
           name,
           password: hashPassword,
@@ -28,13 +28,50 @@ export async function RoutesPublic(app: FastifyInstance) {
       })
       res.status(201)
       return { message: "user create sucess!", user }
-      
+
     } catch (error) {
       console.log(error)
+      res.status(500)
+    }
+  })
+
+  // Login
+  app.post('/login', async (req, res) => {
+    try {
+      const bodySchema = z.object({
+        email: z.string().email(),
+        password: z.string(),
+      })
+
+      const { email, password } = bodySchema.parse(req.body)
+
+      // Verifica se o usuário existe dentro do banco
+      const userLogin = await prisma.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      if (!userLogin) {
+        return res.status(404).send({ message: 'User not found!' })
+      }
+
+      // Compara a senha do banco com oque o usuário digitou 
+      const isMatch = await bcrypt.compare(password, userLogin.password)
+
+      if (!isMatch) {
+        return res.status(400).send({ message: 'Invalid password!' })
+      }
+
+      return res.status(200).send({ message: 'Login successful!', userLogin });
+
+    } catch (error) {
+      console.log(error)
+      res.status(500)
     }
   })
 }
 
 // fnatanieljunior
-//aYpEWPXKuH8ZYXNs
+//  aYpEWPXKuH8ZYXNs
 //
