31e8695f47502c3b4bc09e6d4881751cac1068f3
nattanjunior <fnatanieljunior@gmail.com>
2024-08-22 15:16:48 -0300

    create: middleware autentication of routes

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,7 +4,20 @@ import { Request, Response, NextFunction } from 'express'
 const JWT_SECRET = process.env.JWT_SECRET as string
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req)
+
+  const token = req.headers.authorization
+  console.log(token)
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso Negado' })
+  }
+
+  try {
+    const decode = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
+    console.log(decode)
+  } catch (error) {
+    return res.status(401).json({message: 'Tokem inválido'})
+  }
   next()
 }
 
